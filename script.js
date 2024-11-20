d9bd97b7600ca11c36f583377bb9d5333cfe4a56
Gabriel Cruz <37374988+devgabrielcruz@users.noreply.github.com>
2024-11-20 05:48:56 -0300

    front-end

---
+++ script.js
@@ -0,0 +1,12 @@
+function removeBloqueio() {
+    const emailPaciente = document.querySelector('#emailPct');
+
+    // validações
+    if (emailPaciente.value == '') {
+        alert('Por favor, digite um endereço de e-mail.');
+    } else {
+        alert(`...`);
+    }
+}
+
+
